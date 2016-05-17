728e2ab2143fac4771d46b2513f5ac92b15f3f60
hsavelli <henrique.savelli@gmail.com>
2016-05-17 17:50:28 -0300

    CSV Importer
    Created a new CSV importer class
    Created a .jar file with all classes

---
+++ src/main/java/net/sf/jabref/importer/csvimport/CsvImporter.java
@@ -0,0 +1,52 @@
+/*
+    Custom class developed by the students of the Software Enginieering class - 2016/1 - UFSCar
+*/
+package net.sf.jabref.importer.csvimport;
+
+import java.io.*;
+import java.util.*;
+
+import net.sf.jabref.importer.ImportFormatReader;
+import net.sf.jabref.importer.OutputPrinter;
+import net.sf.jabref.importer.fileformat.ImportFormat;
+import net.sf.jabref.model.entry.BibEntry;
+import net.sf.jabref.model.entry.BibtexEntryTypes;
+
+public class CsvImporter extends ImportFormat {
+
+    @Override
+    public String getFormatName() {
+        return "CSV Importer";
+    }
+
+    @Override
+    public boolean isRecognizedFormat(InputStream stream) throws IOException {
+        return true;
+    }
+
+    @Override
+    public List<BibEntry> importEntries(InputStream stream, OutputPrinter printer) throws IOException {
+        List<BibEntry> bibitems = new ArrayList<>();
+        BufferedReader in = new BufferedReader(ImportFormatReader.getReaderDefaultEncoding(stream));
+
+        String line = in.readLine();
+        while (line != null) {
+            if (!line.trim().isEmpty()) {
+                String[] fields = line.split(";");
+                BibEntry be = new BibEntry();
+                be.setType(BibtexEntryTypes.TECHREPORT);
+                be.setField("year", fields[0]);
+                be.setField("author", fields[1]);
+                be.setField("title", fields[2]);
+                bibitems.add(be);
+                line = in.readLine();
+            }
+        }
+        return bibitems;
+    }
+
+    @Override
+    public String getExtensions() {
+        return "csv";
+    }
+}
